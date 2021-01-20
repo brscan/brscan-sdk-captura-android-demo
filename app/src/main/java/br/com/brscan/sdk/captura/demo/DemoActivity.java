d9a0c9c795d59554bfd80f981339721f2cb1da89
brscan <lucas@brscan.com.br>
2021-01-20 17:06:16 -0300

    demo banco original

---
+++ app/src/main/java/br/com/brscan/sdk/captura/demo/DemoActivity.java
@@ -0,0 +1,94 @@
+package br.com.brscan.sdk.captura.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.com.brscan.sdk.captura.CapturaActivity;
+
+public class DemoActivity extends AppCompatActivity {
+    private static final String TAG = "BrScanSDKDemoActivity";
+    private static final int RETORNO_CAPTURA_ACTIVITY = 10001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1001);
+
+        ImageView capturaDocumento = findViewById(R.id.capturaDocumento);
+        capturaDocumento.setOnTouchListener(new Button.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    iniciaCaptura();
+                    return true;
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    private void iniciaCaptura() {
+        Intent i = new Intent(DemoActivity.this, CapturaActivity.class);
+        i.putExtra("chave", "1325927C99BF0EEA791F96CECE5D1D5B8BAC859D16CAABE4");
+        i.putExtra("cropDocumento", true);
+        i.putExtra("validaDocumento", true);
+        startActivityForResult(i, RETORNO_CAPTURA_ACTIVITY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case (RETORNO_CAPTURA_ACTIVITY) : {
+                if(resultCode == CapturaActivity.SUCESSO_CAPTURA_IMAGEM) {
+                    String arquivo = data.getStringExtra("arquivo");
+                    Bitmap imageBitmap = BitmapFactory.decodeFile(arquivo);
+
+                    LinearLayout capturaDocumentoNovo = findViewById(R.id.capturaDocumentoNovo);
+                    capturaDocumentoNovo.setVisibility(View.VISIBLE);
+
+                    ImageView imagemCapturada = findViewById(R.id.imagemCapturada);
+                    imagemCapturada.setImageBitmap(imageBitmap);
+
+                    String tipo = data.getStringExtra("tipo");
+
+                    if(tipo != null && tipo != "") {
+                        TextView tipoTexto = findViewById(R.id.tipoTexto);
+                        tipoTexto.setText("Tipo: "+tipo);
+                    }
+
+                    String score = data.getStringExtra("score");
+
+                    if(score != null && score != "") {
+                        TextView scoreTexto = findViewById(R.id.scoreTexto);
+                        scoreTexto.setText("Score: "+score);
+                    }
+                }
+                else {
+                    String erro = data.getStringExtra("erro");
+                    Toast.makeText(this, erro, Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+        }
+    }
+}
